03f41354f4831f0c19639cf3471f4b28c1f1f3bf
icxd <icoding666@gmail.com>
2025-05-11 23:47:13 +0200

    update `simple.c` to use new instructions

--- simple.c
+++ simple.c
@@ -6,16 +6,23 @@ int main(void) {
   rs_init(&rs, RS_TARGET_AARCH64_MACOS_GAS);
 
   size_t entry_bb = rs_append_basic_block(&rs, "entry");
-  size_t exit_bb = rs_append_basic_block(&rs, NULL);
+  size_t then_bb = rs_append_basic_block(&rs, NULL);
+  size_t else_bb = rs_append_basic_block(&rs, NULL);
 
   rs_position_at_basic_block(&rs, entry_bb);
   rs_operand_t load34 = rs_build_load(&rs, RS_OPERAND_INT64(34));
-  rs_operand_t load35 = rs_build_load(&rs, RS_OPERAND_INT64(35));
+  rs_operand_t load35 = rs_build_load(&rs, RS_OPERAND_INT64(36));
   rs_operand_t add_result = rs_build_add(&rs, load34, load35);
-  rs_build_br(&rs, RS_OPERAND_BB(exit_bb));
+  rs_operand_t cmp_result =
+      rs_build_cmp_eq(&rs, add_result, RS_OPERAND_INT64(69));
+  rs_build_br_if(&rs, cmp_result, RS_OPERAND_BB(then_bb),
+                 RS_OPERAND_BB(else_bb));
 
-  rs_position_at_basic_block(&rs, exit_bb);
+  rs_position_at_basic_block(&rs, then_bb);
   rs_build_ret(&rs, add_result);
+
+  rs_position_at_basic_block(&rs, else_bb);
+  rs_build_ret(&rs, RS_OPERAND_INT64(123));
 
   {
     FILE *fp = fopen("simple.S", "w");
